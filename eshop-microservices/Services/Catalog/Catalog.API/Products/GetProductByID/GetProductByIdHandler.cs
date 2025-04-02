3695697751905df68dc7a9f97cc12a168852f619
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-02 16:01:26 +0200

    Create GetProductByIdHandler.cs

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductByID/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductByID;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdQueryResult>;
+public record GetProductByIdQueryResult(Product product);
+
+public class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdEndpoint> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdQueryResult>
+{
+    public async Task<GetProductByIdQueryResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"GetProductByIdQueryHandler.Handle called with {query}");
+
+        var product = await session.Query<Product>()
+            .Where(q => q.Id == query.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+        return new GetProductByIdQueryResult(product);
+    }
+
+}
