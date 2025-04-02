0bfe643511db208cf186d41796f1c7ce7151262b
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-02 11:16:21 +0200

    create Get Products Endpoint

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductsRequest();
+public record GetProductResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var query = new GetProductsQuery();
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetProductResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductResponse>()
+        .WithSummary("Get all products")
+        .WithDescription("Retrieves all products from the database.");
+    }
+}
