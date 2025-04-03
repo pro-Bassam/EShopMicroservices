0a80c476da1281cf9d3256dc125dbf82eead77e9
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-03 21:08:31 +0200

    Create GetProductByIdEndpoint.cs

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProductByID/GetProductByIdEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductByID;
+
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+public class GetProductByIdEndpoint() : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}",
+            async (Guid id, ISender sender) =>
+            {
+                var query = new GetProductByIdQuery(id);
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get product by Id")
+            .WithDescription("Retrieves a product from the database.");
+    }
+}
