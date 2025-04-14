7edce35606661ae9302be34ba357ec7e7a392aef
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-15 01:28:15 +0200

    Update Program.cs

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,21 +1,40 @@
+﻿using BuildingBlocks.Behaviors;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container DI
+// Configure Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddFilter("Npgsql", LogLevel.Warning); // suppress verbose Npgsql info logs
+
+// Add Services to DI
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config =>
+
+var assembly = Assembly.GetExecutingAssembly(); // NOTE: It is the same as typeof(Program).Assembly;
+
+// MediatR + Pipeline Behaviors
+builder.Services.AddMediatR(cfg =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.RegisterServicesFromAssembly(assembly);
+    cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
 });
+
+// FluentValidation: register all validators in this assembly
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+// Marten (PostgreSQL as document store)
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
-}).UseLightweightSessions();
+})
+    .UseLightweightSessions();
 
-// Configure logging to suppress Npgsql logs
-builder.Logging.AddFilter("Npgsql", LogLevel.None);
-
+// Build the app
 var app = builder.Build();
 
+// Middleware pipeline
+// NOTE: MediatR pipeline behaviors are not part of the ASP .NET Core HTTP middleware pipeline
 // Configure the HTTP request pipeline
 app.MapCarter();
 
