a287aba4f54f00707e1426aa376ac145940d13ec
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-15 01:28:07 +0200

    Create Product Command Validator

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -5,20 +5,53 @@
         List<string> Category,
         string Description,
         string ImageFile,
-        decimal Price)
-        : ICommand<CreateProductResult>;
+        decimal Price
+    ) : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session)
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Product name is required.");
+
+            RuleFor(x => x.Category)
+                .NotEmpty()
+                .WithMessage("Product category is required.");
+
+            RuleFor(x => x.ImageFile)
+                .NotEmpty()
+                .WithMessage("ImageFile is required.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(
+        IDocumentSession session,
+        ILogger<CreateProductCommandHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(
+            CreateProductCommand command,
+            CancellationToken cancellationToken
+        )
         {
             // Business logic to create a product
             // create a product entity from command object
             // save the product entity to the database
             // return the CreateProductResult result
+
+            logger.LogInformation(
+                "Creating product: {Name} in categories {Categories}",
+                command.Name,
+                string.Join(",", command.Category)
+            );
 
             var product = new Product
             {
@@ -29,12 +62,14 @@
                 Price = command.Price
             };
 
-            // TODO
-            // Save the product entity to the database
             session.Store(product); // to save the product as a document database
             await session.SaveChangesAsync(cancellationToken);
 
-            // return result
+            logger.LogInformation(
+                "Product created with ID: {ProductId}",
+                product.Id
+            );
+
             return new CreateProductResult(product.Id);
         }
     }
