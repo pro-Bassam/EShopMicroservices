2533e9174832cacc9f621f5d52aabb4aee1a9cdb
Bassam Ehab <bassam.bosax10@gmail.com>
2025-04-16 00:45:58 +0200

    added global exception handling

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,8 @@
 ﻿using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,4 +41,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 app.MapCarter();
 
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null) return;
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 app.Run();
