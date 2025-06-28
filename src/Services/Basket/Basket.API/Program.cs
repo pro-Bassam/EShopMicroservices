efbf6cd26b5fc65cbfd1a426afffe3fb06a6f9e4
Bassam Ehab <bassam.bosax10@gmail.com>
2025-06-28 14:55:10 +0300

    Add custom exception handler to Basket.API
    Registered CustomExceptionHandler and enabled exception handling middleware in Program.cs. Also added global using for BuildingBlocks.Exceptions.Handler.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -18,10 +18,12 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
