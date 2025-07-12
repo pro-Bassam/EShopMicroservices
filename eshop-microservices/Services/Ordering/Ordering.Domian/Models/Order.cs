27a3d28653ab83cfdc6ec592a9dc40fc09322759
Bassam Ehab <bassam.bosax10@gmail.com>
2025-07-12 18:27:56 +0300

    Add domain events for order creation and update
    Introduced OrderCreatedEvent and OrderUpdatedEvent records to represent domain events for order creation and update. Updated Order model to use these new events and adjusted global usings accordingly.

--- eshop-microservices/Services/Ordering/Ordering.Domian/Models/Order.cs
+++ eshop-microservices/Services/Ordering/Ordering.Domian/Models/Order.cs
@@ -35,7 +35,7 @@ public class Order : Aggregate<OrderId>
             Status = OrderStatus.Pending
         };
 
-        order.AddDomainEvent(new OrderCreateEvent(order));
+        order.AddDomainEvent(new OrderCreatedEvent(order));
 
         return order;
     }
@@ -49,7 +49,7 @@ public class Order : Aggregate<OrderId>
         Payment = payment;
         Status = status;
 
-        AddDomainEvent(new OrderUpdateEvent(this));
+        AddDomainEvent(new OrderUpdatedEvent(this));
     }
 
     public void Add(ProductId productId, int quantity, decimal price)
