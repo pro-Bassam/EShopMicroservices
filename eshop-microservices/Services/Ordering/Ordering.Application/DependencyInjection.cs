13f272eb600663784734035cfb32b7d3fc5a24b2
Bassam Ehab <bassam.bosax10@gmail.com>
2025-09-20 13:46:21 +0300

    Register MediatR and interceptors in DI setup
    Added MediatR registration in Ordering.Application's dependency injection for CQRS support. In Ordering.Infrastructure, refactored DbContext registration to use DI for ISaveChangesInterceptor implementations, enabling multiple interceptors (AuditableEntityInterceptor and DispatchDomainEventsInterceptor) to be injected and used.

--- eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application
 {
@@ -8,6 +9,11 @@ namespace Ordering.Application
         {
             // Register application services here
             // Example: services.AddScoped<IOrderService, OrderService>();
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
             return services;
         }
     }
