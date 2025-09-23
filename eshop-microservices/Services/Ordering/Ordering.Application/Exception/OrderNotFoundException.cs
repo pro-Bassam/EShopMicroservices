a533ea3c9584779c2549110514deed20dcb510da
Bassam Ehab <bassam.bosax10@gmail.com>
2025-09-23 21:35:09 +0300

    Add OrderNotFoundException to Ordering.Application
    Introduces a custom exception for handling cases where an order is not found, inheriting from NotFoundException. This improves error handling and clarity in the ordering service.

---
+++ eshop-microservices/Services/Ordering/Ordering.Application/Exception/OrderNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Ordering.Application.Exception;
+
+public class OrderNotFoundException : NotFoundException
+{
+    public OrderNotFoundException(Guid id) : base("Order", id) { }
+}
+
