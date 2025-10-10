689c169b90772fa44fddf9346085df6b1ac25c30
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-10 17:30:42 +0300

    Add domain event handlers for order events
    Introduced OrderCreatedEventHandler and OrderUpdatedEventHandler to handle domain events for order creation and updates. Updated global usings and project folders to support new event handlers.

--- eshop-microservices/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,7 +1,10 @@
 ﻿global using BuildingBlocks.CQRS;
 global using FluentValidation;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exception;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
