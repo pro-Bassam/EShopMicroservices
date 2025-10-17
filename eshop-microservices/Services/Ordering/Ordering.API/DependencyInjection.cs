ad350bc1fcea3332b49b028c041f50c3a9b539bd
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-17 14:36:50 +0300

    Add health checks to Ordering.API
    Integrated SQL Server health checks and health check UI response in Ordering.API. Updated dependency injection to accept configuration and registered necessary NuGet packages for health checks.

--- eshop-microservices/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ eshop-microservices/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,16 +1,20 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             // Register application services here
             // Example: services.AddScoped<IOrderService, OrderService>();
             services.AddCarter();
 
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -22,6 +26,11 @@ namespace Ordering.API
             app.MapCarter();
 
             app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return app;
         }
