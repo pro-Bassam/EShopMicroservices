015d73903ccbc9320f601abcf726b9d92ead97bd
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-18 15:49:24 +0300

    Implement BasketCheckoutEventHandler logic
    Replaces the NotImplementedException in BasketCheckoutEventHandler with logic to map the incoming BasketCheckoutEvent to a CreateOrderCommand and send it using ISender. Adds logging and a mapping method with placeholder order items for demonstration.

--- eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -1,14 +1,45 @@
 ﻿using BuildingBlocks.Messaging.Events;
 using MassTransit;
+using Ordering.Application.Orders.Commands.CreateOrder;
 
 namespace Ordering.Application.Orders.EventHandlers.Integration;
-
 public class BasketCheckoutEventHandler
+    (ISender sender, ILogger<BasketCheckoutEventHandler> logger)
     : IConsumer<BasketCheckoutEvent>
 {
-    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
     {
-        throw new NotImplementedException();
+        // TODO: Create new order and start order fulfillment process
+        logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+        var command = MapToCreateOrderCommand(context.Message);
+        await sender.Send(command);
+    }
+
+    private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+    {
+        // Create full order with incoming event data
+        var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+        var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.CVV, message.PaymentMethod);
+        var orderId = Guid.NewGuid();
+
+        var orderDto = new OrderDto(
+            Id: orderId,
+            CustomerId: message.CustomerId,
+            OrderName: message.UserName,
+            ShippingAddress: addressDto,
+            BillingAddress: addressDto,
+            Payment: paymentDto,
+            Status: Ordering.Domain.Enums.OrderStatus.Pending,
+            // Order data is not included in the event, so using hardcoded items for demo purposes
+            // in real scenario, you would retrieve basket items from basket service or database
+            // Todo: Implement retrieval of basket items
+            OrderItems:
+            [
+                new OrderItemDto(orderId, new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"), 2, 500),
+                new OrderItemDto(orderId, new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"), 1, 400)
+            ]);
+
+        return new CreateOrderCommand(orderDto);
     }
 }
-
