670f7ed029c4f0b88ed5ea4e029731685e8812de
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-18 15:34:46 +0300

    Add BasketCheckoutEventHandler integration event
    Introduces BasketCheckoutEventHandler to handle BasketCheckoutEvent via MassTransit. Removes unused folder reference from the project file.

---
+++ eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler
+    : IConsumer<BasketCheckoutEvent>
+{
+    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
