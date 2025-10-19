9d543b11db20937d8e66260cb352299284078bd7
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-19 22:47:16 +0300

    Add OrderDto conversion and event publishing logic
    Added `ToOrderDto` and `DtoFromOrder` methods in `OrderExtensions` to map `Order` to `OrderDto`. Updated `OrderCreatedEventHandler` to use `IPublishEndpoint` for publishing integration events. Refactored `Handle` method to log domain events and publish `OrderDto`-based integration events. Removed placeholder `Task.CompletedTask` return.

--- eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,19 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+﻿using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    public class OrderCreatedEventHandler
+        (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
         : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domanEvent, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-            return Task.CompletedTask;
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domanEvent.GetType().Name);
+
+            // perform publishing integration event
+            var orderCreatedIntegrationEvent = domanEvent.order.ToOrderDto();
+
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken)
         }
     }
 }
