b0c9fdb735c6c20b981aa3869b4322b4258e6852
Bassam Ehab <bassam.bosax10@gmail.com>
2025-10-19 23:09:56 +0300

    Add feature flag for conditional event publishing
    Introduced a `FeatureManagement` section in `appsettings.json` to enable dynamic control of the `OrderFulfillment` feature. Integrated the `Microsoft.FeatureManagement` library and registered `IFeatureManager` in the dependency injection container. Updated `OrderCreatedEventHandler` to use the feature flag, conditionally publishing integration events based on the `OrderFulfillment` feature state.

--- eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,19 +1,22 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain
 {
     public class OrderCreatedEventHandler
-        (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+        (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
         : INotificationHandler<OrderCreatedEvent>
     {
         public async Task Handle(OrderCreatedEvent domanEvent, CancellationToken cancellationToken)
         {
             logger.LogInformation("Domain Event handled: {DomainEvent}", domanEvent.GetType().Name);
 
-            // perform publishing integration event
-            var orderCreatedIntegrationEvent = domanEvent.order.ToOrderDto();
-
-            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken)
+            // If feature enabled perform integration event publishing 
+            if (await featureManager.IsEnabledAsync("OrderFulfillment"))
+            {
+                var orderCreatedIntegrationEvent = domanEvent.order.ToOrderDto();
+                await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            }
         }
     }
 }
